5cf502bcdbd718263ecfe3b6bd3b9ecd8c3647af
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-11-20 18:46:00 +0530

    Fetched Movies Popular Api from TMDB.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const LOGO = 
+    export const LOGO = 
       " https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940 "
 
 
@@ -8,3 +8,10 @@ export const Background_IMage =
         "https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/8eb1e781-3494-4aa4-9405-268ca6473e4c/IN-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg" 
                     
 
+export const API_Options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDg4MzA4ZjBhMjdkYTBhZjBlZjkxMWRkM2MzNTg0MiIsInN1YiI6IjY1NDhjNDlhNDFhNTYxMzM2Yjc4YzZhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q2zmf6BbdBoczsoguKJ4iWdtcJU8yRDlttHy_jBT3I4'
+            }
+          };
