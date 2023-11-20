178d942d65e8fc540cc5a28222152c11aa5777df
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-11-20 19:13:27 +0530

    Integrated Movie Api with the Redux Store by creating movieSLice.

---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,20 @@
+// all movies data will be inside this 
+// this data is passed to redux store.
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+    name : "movies",
+    initialState:{
+        addNowPlayingMovies : null,
+    },
+    reducers: {
+        addNowPlayingMovies : (state,action) => {
+            state.addNowPlayingMovies = action.payload ; 
+        }
+    }
+});
+
+export const {addNowPlayingMovies} = moviesSlice.actions;
+
+export default moviesSlice.reducer;
