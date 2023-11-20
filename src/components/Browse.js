294e868c4c7920c38d5fe89cc7808fc0d9988933
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-11-20 19:41:34 +0530

    Creating You own custom hook for fetching api.

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,22 +1,13 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
-import { API_Options } from '../utils/constants'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/moviesSlice'
+import useNowPlayingMovies from '../CustomHooks/useNowPlayingMovies'
 
 const Browse = () => {
 
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async() =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',
-    API_Options)
-    const json = await data.json()
-    console.log(json);
-    dispatch(addNowPlayingMovies(json.results));
-  };
-  useEffect( () => {
-    getNowPlayingMovies();
-  },[])
+  useNowPlayingMovies();
+
+
+
   return (
     <div>
       <Header/>
@@ -24,4 +15,4 @@ const Browse = () => {
   )
 }
 
-export default Browse
+export default Browse;
