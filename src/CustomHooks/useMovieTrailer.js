088a8f4b283a579abe9707b56a8840257de324c8
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-11-26 14:35:24 +0530

    UnderStanding Memoization of data in our apps.

--- src/CustomHooks/useMovieTrailer.js
+++ src/CustomHooks/useMovieTrailer.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { API_Options } from '../utils/constants';
 import { addTrailerVideo } from '../utils/moviesSlice';
 import { useEffect } from 'react';
 
 const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
+
+    const trailerVideo = useSelector(
+      (store) => store.movies.trailerVideo
+    );
 
     const getMovieVideos = async() => {
               const data = await fetch("https://api.themoviedb.org/3/movie/"
@@ -22,7 +26,7 @@ const useMovieTrailer = (movieId) => {
   
     };
     useEffect(() => {
-      getMovieVideos();
+      !trailerVideo && getMovieVideos();
     },[]);
 }
 
--- src/CustomHooks/useNowPlayingMovies.js
+++ src/CustomHooks/useNowPlayingMovies.js
@@ -1,21 +1,31 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_Options } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useNowPlayingMovies = () => {
-    const dispatch = useDispatch();
-    const getNowPlayingMovies = async() =>{
-      const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',
-      API_Options)
-      const json = await data.json()
+  // Fetch Data from TMDB API and update store
+  const dispatch = useDispatch();
 
-      dispatch(addNowPlayingMovies(json.results));
-    };
-    useEffect( () => {
-      getNowPlayingMovies();
-    },[]);
-  
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+      API_Options
+    );
+    const json = await data.json();
+    console.log('Now playing api called');
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    !nowPlayingMovies && getNowPlayingMovies();
+    // getNowPlayingMovies();
+  }, []);
 };
+
 
 export default useNowPlayingMovies;
