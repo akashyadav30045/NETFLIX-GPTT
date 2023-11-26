bd64d76b1f2b73e1e9810146c788731bcae0df3b
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-11-26 13:23:50 +0530

    Showing Movie Suggestions on Screen.

--- src/components/GptMoviesuggestions.js
+++ src/components/GptMoviesuggestions.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieList from "./MovieList"
+
 
 const GptMoviesuggestions = () => {
+  const gpt=useSelector(store=>store.gpt);
+  const{movieResults,movieNames} = gpt;
+
+  if (!movieNames) return null;
+
+
   return (
-    <div>
-      movie suggestions
+    <div className="p-4 m-4 bg-black text-white opacity-90">
+      {movieNames.map((movieName,index) => (
+        <MovieList  
+          key ={movieName} 
+          title={movieName}   
+          movies={movieResults[index]} 
+          />
+        ))}
     </div>
   )
 }
--- src/components/GptSearchPage.js
+++ src/components/GptSearchPage.js
@@ -6,7 +6,7 @@ import { Background_IMage } from '../utils/constants'
 const GptSearchPage = () => {
   return (
     <div>
-       <div className=" absolute -z-10 " >
+       <div className=" fixed -z-10 " >
         <img 
         src={Background_IMage}
         alt="background-image" />
